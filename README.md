ee9e09df4b21062acdda76083dd816e370a5f2c5
Anastasiya <personalnastya8@gmail.com>
2025-04-19 00:11:37 +0300

    Detekt

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # shopping_list_android_app
+
+
+# Detekt
+
+In case if you face with issue :
+permission denied: ./gradlew
+
+Run command
+
+```
+ chmod +x gradlew
+```
+
+To check lint issues
+
+```
+./gradlew detekt
+```
+
+In folder `build/reports/detekt/detekt.html`
+
+Report details will be located
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,38 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("io.gitlab.arturbosch.detekt").version("1.23.8")
+}
+
+dependencies {
+    detekt(libs.detekt.cli)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$projectDir/config/detekt-config.yml")
+}
+
+tasks.withType<Detekt>().configureEach {
+    setSource(files("app/src/main/java", "app/src/main/java"))
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        sarif.required.set(true)
+        md.required.set(true)
+    }
+    exclude("**/build/**")
+    exclude("**/resources/**")
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "1.8"
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = "1.8"
 }
