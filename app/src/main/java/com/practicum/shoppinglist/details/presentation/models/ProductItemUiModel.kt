468ba29f7f466ecefa6f7a3e1eb271db70404a14
Veetver <khabiev2012@yandex.ru>
2025-04-25 22:02:26 +0300

    Добавлен элемент списка товаров

---
+++ app/src/main/java/com/practicum/shoppinglist/details/presentation/models/ProductItemUiModel.kt
@@ -0,0 +1,8 @@
+package com.practicum.shoppinglist.details.presentation.models
+
+class ProductItemUiModel(
+    val id: Long,
+    val name: String,
+    val amount: String,
+    val completed: Boolean,
+)
