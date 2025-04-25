8683343067c6bb91bcca922902a5a60457b2b2aa
Anastasiya <personalnastya8@gmail.com>
2025-04-25 23:32:29 +0300

    IT-1.1

--- app/src/main/java/com/practicum/shoppinglist/di/NetworkModule.kt
+++ app/src/main/java/com/practicum/shoppinglist/di/NetworkModule.kt
@@ -1,9 +1,9 @@
 package com.practicum.shoppinglist.di
 
 import com.android.ktorsample.feature.http.data.network.AuthResponse
-import com.practicum.shoppinglist.main.domain.api.auth.AuthorizationKtorNetworkClient
+import com.practicum.shoppinglist.main.data.impl.auth.AuthorizationKtorNetworkClient
 import com.practicum.shoppinglist.core.data.network.HttpKtorNetworkClient
-import com.practicum.shoppinglist.main.domain.api.auth.dto.AuthRequest
+import com.practicum.shoppinglist.main.data.impl.auth.dto.AuthRequest
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
