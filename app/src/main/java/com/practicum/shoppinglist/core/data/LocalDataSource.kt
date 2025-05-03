1c27b931eedddcc61d89c9e5f6af083c236c29e4
Veetver <khabiev2012@yandex.ru>
2025-05-03 15:53:13 +0300

    DS return type fix

--- app/src/main/java/com/practicum/shoppinglist/core/data/LocalDataSource.kt
+++ app/src/main/java/com/practicum/shoppinglist/core/data/LocalDataSource.kt
@@ -7,17 +7,17 @@ import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
     fun getAllLists(): Flow<List<ListEntity>>
-    suspend fun insertList(name: String, icon: Long)
-    suspend fun updateList(item: ListEntity)
+    suspend fun insertList(name: String, icon: Long): Long
+    suspend fun updateList(item: ListEntity): Long
     suspend fun getListById(id: Long): ListEntity?
-    suspend fun deleteListById(id: Long)
+    suspend fun deleteListById(id: Long): Long
     fun searchListByName(name: String): Flow<List<ListEntity>>
 
     fun getProductsByListId(id: Long): Flow<List<ProductEntity>>
     suspend fun getProductById(id: Long): ProductEntity?
-    suspend fun insertProduct(listId: Long, item: ProductEntity)
-    suspend fun updateProduct(item: ProductEntity)
-    suspend fun deleteProductById(id: Long)
+    suspend fun insertProduct(listId: Long, item: ProductEntity): Long
+    suspend fun updateProduct(item: ProductEntity): Long
+    suspend fun deleteProductById(id: Long): Long
     suspend fun deleteAll(): Long
     suspend fun deleteAllByListId(id: Long): Long
     suspend fun deleteAllCompletedByListId(id: Long): Long
