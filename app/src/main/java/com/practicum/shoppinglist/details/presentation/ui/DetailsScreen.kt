b60b29556be55a5c6135d48073155a3afc1200e4
Veetver <khabiev2012@yandex.ru>
2025-05-06 20:06:53 +0300

    drag and drop вынесен в отдельный пакет

--- app/src/main/java/com/practicum/shoppinglist/details/presentation/ui/DetailsScreen.kt
+++ app/src/main/java/com/practicum/shoppinglist/details/presentation/ui/DetailsScreen.kt
@@ -1,19 +1,13 @@
 package com.practicum.shoppinglist.details.presentation.ui
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -26,9 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.foundation.lazy.LazyListItemInfo
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CornerSize
@@ -58,22 +49,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -101,6 +86,9 @@ import com.practicum.shoppinglist.core.presentation.ui.FabViewModel
 import com.practicum.shoppinglist.core.presentation.ui.components.SLDropdown
 import com.practicum.shoppinglist.core.presentation.ui.components.SLIconButton
 import com.practicum.shoppinglist.core.presentation.ui.components.SLOutlineTextField
+import com.practicum.shoppinglist.core.presentation.ui.dragDrop.DraggableItem
+import com.practicum.shoppinglist.core.presentation.ui.dragDrop.dragContainer
+import com.practicum.shoppinglist.core.presentation.ui.dragDrop.rememberDragDropState
 import com.practicum.shoppinglist.core.presentation.ui.state.FabIntent
 import com.practicum.shoppinglist.core.presentation.ui.state.FabState
 import com.practicum.shoppinglist.core.presentation.ui.theme.SLTheme
@@ -108,8 +96,6 @@ import com.practicum.shoppinglist.details.presentation.state.DetailsScreenState
 import com.practicum.shoppinglist.details.presentation.viewmodel.DetailsViewModel
 import com.practicum.shoppinglist.details.utils.model.ProductSortOrder
 import com.practicum.shoppinglist.di.api.daggerViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 
@@ -509,161 +495,6 @@ fun DetailsScreenUI(
                 }
             }
         }
-    }
-}
-
-
-@Composable
-fun rememberDragDropState(lazyListState: LazyListState, onMove: (Int, Int) -> Unit): DragDropState {
-    val scope = rememberCoroutineScope()
-    val state = remember(lazyListState) {
-        DragDropState(state = lazyListState, onMove = onMove, scope = scope)
-    }
-    LaunchedEffect(state) {
-        while (true) {
-            val diff = state.scrollChannel.receive()
-            lazyListState.scrollBy(diff)
-        }
-    }
-    return state
-}
-
-class DragDropState internal constructor(
-    private val state: LazyListState,
-    private val scope: CoroutineScope,
-    private val onMove: (Int, Int) -> Unit
-) {
-    var draggingItemIndex by mutableStateOf<Int?>(null)
-        private set
-
-    internal val scrollChannel = Channel<Float>()
-
-    private var draggingItemDraggedDelta by mutableFloatStateOf(0f)
-    private var draggingItemInitialOffset by mutableIntStateOf(0)
-    internal val draggingItemOffset: Float
-        get() =
-            draggingItemLayoutInfo?.let { item ->
-                draggingItemInitialOffset + draggingItemDraggedDelta - item.offset
-            } ?: 0f
-
-    private val draggingItemLayoutInfo: LazyListItemInfo?
-        get() = state.layoutInfo.visibleItemsInfo.firstOrNull { it.index == draggingItemIndex }
-
-    internal var previousIndexOfDraggedItem by mutableStateOf<Int?>(null)
-        private set
-
-    internal var previousItemOffset = Animatable(0f)
-        private set
-
-    internal fun onDragStart(offset: Offset) {
-        state.layoutInfo.visibleItemsInfo
-            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
-            ?.also {
-                draggingItemIndex = it.index
-                draggingItemInitialOffset = it.offset
-            }
-    }
-
-    internal fun onDragInterrupted() {
-        if (draggingItemIndex != null) {
-            previousIndexOfDraggedItem = draggingItemIndex
-            val startOffset = draggingItemOffset
-            scope.launch {
-                previousItemOffset.snapTo(startOffset)
-                previousItemOffset.animateTo(
-                    0f,
-                    spring(stiffness = Spring.StiffnessMediumLow, visibilityThreshold = 1f)
-                )
-                previousIndexOfDraggedItem = null
-            }
-        }
-        draggingItemDraggedDelta = 0f
-        draggingItemIndex = null
-        draggingItemInitialOffset = 0
-    }
-
-    internal fun onDrag(offset: Offset) {
-        draggingItemDraggedDelta += offset.y
-
-        val draggingItem = draggingItemLayoutInfo ?: return
-        val startOffset = draggingItem.offset + draggingItemOffset
-        val endOffset = startOffset + draggingItem.size
-        val middleOffset = startOffset + (endOffset - startOffset) / 2f
-
-        val targetItem =
-            state.layoutInfo.visibleItemsInfo.find { item ->
-                middleOffset.toInt() in item.offset..item.offsetEnd &&
-                        draggingItem.index != item.index
-            }
-        if (targetItem != null) {
-            if (
-                draggingItem.index == state.firstVisibleItemIndex ||
-                targetItem.index == state.firstVisibleItemIndex
-            ) {
-                state.requestScrollToItem(
-                    state.firstVisibleItemIndex,
-                    state.firstVisibleItemScrollOffset
-                )
-            }
-            onMove.invoke(draggingItem.index, targetItem.index)
-            draggingItemIndex = targetItem.index
-        } else {
-            val overscroll =
-                when {
-                    draggingItemDraggedDelta > 0 ->
-                        (endOffset - state.layoutInfo.viewportEndOffset).coerceAtLeast(0f)
-
-                    draggingItemDraggedDelta < 0 ->
-                        (startOffset - state.layoutInfo.viewportStartOffset).coerceAtMost(0f)
-
-                    else -> 0f
-                }
-            if (overscroll != 0f) {
-                scrollChannel.trySend(overscroll)
-            }
-        }
-    }
-
-    private val LazyListItemInfo.offsetEnd: Int
-        get() = this.offset + this.size
-}
-
-@Composable
-fun LazyItemScope.DraggableItem(
-    dragDropState: DragDropState,
-    index: Int,
-    modifier: Modifier = Modifier,
-    content: @Composable ColumnScope.(isDragging: Boolean) -> Unit
-) {
-    val dragging = index == dragDropState.draggingItemIndex
-    val draggingModifier =
-        if (dragging) {
-            Modifier
-                .zIndex(1f)
-                .graphicsLayer { translationY = dragDropState.draggingItemOffset }
-        } else if (index == dragDropState.previousIndexOfDraggedItem) {
-            Modifier
-                .zIndex(1f)
-                .graphicsLayer {
-                    translationY = dragDropState.previousItemOffset.value
-                }
-        } else {
-            Modifier.animateItem(fadeInSpec = null, fadeOutSpec = null)
-        }
-    Column(modifier = modifier.then(draggingModifier)) { content(dragging) }
-}
-
-fun Modifier.dragContainer(dragDropState: DragDropState): Modifier {
-    return pointerInput(dragDropState) {
-        detectDragGesturesAfterLongPress(
-            onDrag = { change, offset ->
-                change.consume()
-                dragDropState.onDrag(offset = offset)
-            },
-            onDragStart = { offset -> dragDropState.onDragStart(offset) },
-            onDragEnd = { dragDropState.onDragInterrupted() },
-            onDragCancel = { dragDropState.onDragInterrupted() }
-        )
     }
 }
 
