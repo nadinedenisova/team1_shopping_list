8b08ffa815cb3384ffd1e6db55a905276dd637c3
Anastasiya <personalnastya8@gmail.com>
2025-05-06 19:24:08 +0300

    IT-3.3

--- app/src/main/java/com/practicum/shoppinglist/auth/ui/LoginScreen.kt
+++ app/src/main/java/com/practicum/shoppinglist/auth/ui/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -64,11 +65,11 @@ fun LoginForm(
     navController: NavController,
     onLoginClick: (email: String, password: String) -> Unit,
 ) {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
 
-    var isEmailError by remember { mutableStateOf(false) }
-    var isPasswordError by remember { mutableStateOf(false) }
+    var isEmailError by rememberSaveable { mutableStateOf(false) }
+    var isPasswordError by rememberSaveable { mutableStateOf(false) }
 
     val isEmailValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     val isPasswordValid = password.isNotEmpty()
